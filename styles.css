562e4c32d9907f848bc312fc0cfd843c5b42195e
Aidenniknam <paper.niknam@gmail.com>
2025-02-23 22:53:45 -0600

    Add files via upload

---
+++ styles.css
@@ -0,0 +1,12 @@
+// انیمیشن تغییر نقش‌های شغلی
+const roles = ["IT Systems Administrator", "Cybersecurity Specialist", "Cloud Enthusiast", "DevOps Learner"];
+let index = 0;
+let dynamicTextElement = document.querySelector(".dynamic-text");
+
+function updateDynamicText() {
+    dynamicTextElement.innerText = roles[index];
+    index = (index + 1) % roles.length;
+}
+
+setInterval(updateDynamicText, 3000);
+updateDynamicText();
